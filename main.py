e0b82f4e63db206d8d363f91006c30c725e9b3a2
alvin <aocasiojr@my.nl.edu>
2023-10-10 15:28:51 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+current_time = int(input("what is the current time(in hours)?"))
+wait_time = int(input("how many hours do you want to wait"))
+
+print(current_time)
+print(wait_time)
+final_time = current_time + wait_time
+print(final_time)
